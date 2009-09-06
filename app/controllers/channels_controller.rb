76cbfda9631dc0cadd8aa87977b8d595a0905d39
dblugeon <d@blugeon>
2009-09-07 00:03:43 +0200

    factorisation method check_channel

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -15,18 +15,36 @@ class ChannelsController < ApplicationController
   end
 
   def show
+    check_channel
   end
 
   def edit
-    self.logger.debug "value of params[:id] = #{params[:id]}"
-    @channel = eyetv_instance.find_by_id(:channel, params[:id].to_i)
-    self.logger.debug "value of @channel = #{@channel}"
+    check_channel
   end
 
   def update
+    check_channel
+    logger.debug "value of params #{params} to merge"
+    if (params[:enabled] == nil)
+      @channel.enabled= false
+    else params[:enabled]
+      @channel.enabled= true
+    end
+    @channel.name = params[:name]
+    redirect_to :action=>"index"
   end
 
   def destroy
   end
 
+  private
+  def check_channel
+    self.logger.debug "value of params[:id] = #{params[:id]}"
+    @channel = eyetv_instance.find_by_id(:channel, params[:id].to_i)
+    self.logger.debug "value of @channel = #{@channel}"
+    if @channel == nil
+      render :file => "#{RAILS_ROOT}/public/404.html",
+        :status => 404 and return
+    end
+  end
 end
