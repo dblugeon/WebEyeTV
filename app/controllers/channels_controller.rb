82d345a29eddd95afbefbeb0e9fe3594fa9b9be8
dblugeon <d@blugeon>
2009-09-06 11:50:51 +0200

    add early support channel. Work In Progress

---
+++ app/controllers/channels_controller.rb
@@ -0,0 +1,24 @@
+class ChannelsController < ApplicationController
+  def list
+    @channels = eyetv_instance.channels
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
