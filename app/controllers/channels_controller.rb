1f5d9f4c3bfbc59ced8303eca0182844af534130
dblugeon <d@blugeon>
2009-09-07 12:24:00 +0200

    add map.resources for channels remove show action and view usage url path helpers for REST in link_to

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,21 +1,7 @@
 class ChannelsController < ApplicationController
 
   def index
-    redirect_to :action => "list"
-  end
-
-  def list
     @channels = eyetv_instance.channels
-  end
-
-  def new
-  end
-
-  def create
-  end
-
-  def show
-    check_channel
   end
 
   def edit
@@ -24,7 +10,6 @@ class ChannelsController < ApplicationController
 
   def update
     check_channel
-    logger.debug "value of params #{params} to merge"
     if (params[:enabled] == nil)
       @channel.enabled= false
     else params[:enabled]
--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ ActionController::Routing::Routes.draw do |map|
   #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
   #     admin.resources :products
   #   end
-
+  map.resources :channels
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   map.root :controller => "channels"
 
