cdf9681c3ba5c3738984709fdf7e2869d73818df
dblugeon <d@blugeon>
2009-09-07 12:36:59 +0200

    add http_authentification_basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ include EyeTV
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
+  before_filter :authenticate
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
@@ -19,5 +20,13 @@ class ApplicationController < ActionController::Base
   protected
   def eyetv_instance
     @eyetv_ref = EyeTV::EyeTV.new if @eyetv_ref == nil
+  end
+
+  def authenticate
+    if APP_CONFIG['perform_authentication']
+      authenticate_or_request_with_http_basic do |username, password|
+        username == APP_CONFIG['username'] && password == APP_CONFIG['password']
+      end
+    end
   end
 end
