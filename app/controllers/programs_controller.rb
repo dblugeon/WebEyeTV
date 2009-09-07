5dc9b08f9184744c395fad0d6a83fa82d665fc18
dblugeon <d@blugeon>
2009-09-07 22:14:31 +0200

    add delete program feature add edit feature add fr local for date, time and units

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -10,6 +10,25 @@ class ProgramsController < ApplicationController
 
   def edit
     check_program
+    @input_sources = EyeTV::Program.input_sources_possible
+    @repeats = EyeTV::Program.repeats_possible
+    @quality = EyeTV::Program.qualities_possible
+    @channels = eyetv_instance.channels
+  end
+
+  def update
+    check_program
+    start_time = Time.local(params[:date][:year], params[:date][:month], params[:date][:day], params[:date][:hour], params[:date][:minute])
+    params[:start_time]=start_time
+    update_program(@program, params)
+    redirect_to programs_url
+  end
+
+  def destroy
+    check_program
+    uid = @program.uid
+    @program.delete
+    redirect_to programs_url
   end
   
   private
@@ -20,6 +39,22 @@ class ProgramsController < ApplicationController
     if @program == nil
       render :file => "#{RAILS_ROOT}/public/404.html",
         :status => 404 and return
+    end
+  end
+
+  def update_program(program, params)
+    if(params)
+      params.each do |key, value|
+        if (program.respond_to?(key) and program.method(key).arity == 1)
+          logger.debug "call #{key} with value : #{value}"
+          program.send(key, value)
+        elsif (program.respond_to?("#{key}=") and program.method("#{key}=").arity == 1)
+          logger.debug "call #{key}= with value : #{value}"
+          program.send("#{key}=", value)
+        else
+          logger.debug "Program class doesn't respond to #{key}"
+        end
+      end
     end
   end
 end
