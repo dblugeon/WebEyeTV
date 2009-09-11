ccf7889a154ed7468e023630afc259f7431256c0
dblugeon <d@blugeon>
2009-09-11 22:19:00 +0200

    update default route to programs

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :programs
   map.resources :recordings
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.root :controller => "channels"
+  map.root :controller => "programs"
 
   # See how all your routes lay out with "rake routes"
 
