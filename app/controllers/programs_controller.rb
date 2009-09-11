3d97294eb892dcd54088855225b78ffa4c082261
dblugeon <d@blugeon>
2009-09-11 22:03:31 +0200

    add support creation of program

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -23,6 +23,47 @@ class ProgramsController < ApplicationController
     redirect_to_404(@program)
   end
 
+  def new
+    @input_sources = EyeTV::Program.input_sources_possible
+    @repeats = EyeTV::Program.repeats_possible
+    @quality = EyeTV::Program.qualities_possible
+    @channels = eyetv_instance.channels
+    @current_channel = eyetv_instance.current_channel_number
+  end
+
+  def create
+    if request.post?
+      new_program_options = {}
+      key_possibles = ["duration", "title", "repeats", "date", "enabled", "channel_number", "description", "input_source", "quality"]
+      params.each do |key, value|
+        if key_possibles.include?(key)
+          if key != "date"
+            new_program_options[key.to_sym] = value
+          else
+            new_program_options[:start_time] = Time.local(params[:date][:year], params[:date][:month],
+                params[:date][:day], params[:date][:hour], params[:date][:minute])
+          end
+        end
+      end
+      if !params.has_key?("enabled")
+        new_program_options["enabled"] = false
+      end
+      logger.debug "new_program_options #{new_program_options.inspect}"
+      begin
+        logger.info "attempt create program"
+        @program = eyetv_instance.make_program(new_program_options)
+      rescue EyeTV::ConflictProgramException => e
+        logger.info "program creation in conflict with #{e.program_exist.title} at #{e.program_exist.start_time}"
+        flash[:error] = t(:program_in_conflict, :title => e.program_exist.title, :start_time => e.program_exist.start_time, :duration =>e.program_exist.duration)
+        redirect_to new_program_url
+      else
+        logger.info "program creation successfull"
+        flash[:notice] = t(:program_crated, :title => @program.title)
+        redirect_to programs_path
+      end
+    end
+  end
+
   def edit
     @program = check_eyetv_object(:program, params[:id])
     redirect_to_404(@program)
