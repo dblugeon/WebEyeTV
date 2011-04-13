d364b2bca73954ae59756efac7fba395dd95ac98
dblugeon <d@blugeon>
2011-04-13 22:22:38 +0200

    remove session_key warning

--- config/environment.rb
+++ config/environment.rb
@@ -56,7 +56,7 @@ Rails::Initializer.run do |config|
   # Make sure the secret is at least 30 characters and all random, 
   # no regular words or you'll be exposed to dictionary attacks.
   config.action_controller.session = {
-    :session_key => '_WebEyeTV_session',
+    :key => '_WebEyeTV_session',
     :secret      => 'f101a0e5f3763a260a9110ffbcdca7c4bc12e11e2be6bfab75eb7e7aa55d2dc839fa7f64bf21df35c71e67ed76d9c44c042a332a915f5cd8fa90f554d52061d6'
   }
 
