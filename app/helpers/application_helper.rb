5e9f49f515221e0e15259b536e3d82e0f8334d43
dblugeon <d@blugeon>
2011-04-14 21:40:34 +0200

    adding todo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,26 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  #generate table with diskusage
+  #To refactor this
+  def diskusage
+    html = "<table>"
+    IO.popen("df -h") do |df_io|
+      html += "<tr>"
+      df_io.gets.split(" ").each do |word|
+        html += "<th>#{word}</th>"
+      end
+      html += "</tr>"
+      while !df_io.eof?
+        line = df_io.gets
+        if line.index(/^\/dev\/disk*/) != nil
+          html += "<tr>"
+          line.split(" ").each do |word|
+            html += "<td>#{word}</td>"
+          end
+          html += "</tr>"
+        end
+      end
+    end
+    html +="</table>"
+  end
 end
